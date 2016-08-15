82663a5f73c823984f9d777ca57f8c3e0ceaf6aa
Shantanu <shantanusvyas@gmail.com>
2016-08-15 12:54:13 -0400

    no tabs

--- src/AreaType.java
+++ src/AreaType.java
@@ -1,4 +1,4 @@
 public enum AreaType
 {
-    LAWNMOWER, SPIRAL
+	LAWNMOWER, SPIRAL
 }
--- src/Path.java
+++ src/Path.java
@@ -2,60 +2,60 @@ import java.util.ArrayList;
 
 public class Path
 {
-	protected ArrayList<LatLng> points = new ArrayList<LatLng>();
-	double transectAngle = 0;
+    protected ArrayList<LatLng> points = new ArrayList<LatLng>();
+    double transectAngle = 0;
 
-	public Path()
-	{
-	}
+    public Path()
+    {
+    }
 
-	public ArrayList<LatLng> getPoints()
-	{
-		return points;
-	}
-	public ArrayList<LatLng> getOriginalPoints()
-	{
-		return points;
-	}
-	public void setPoints(ArrayList<LatLng> list)
-	{
-		points = list;
-	}
-	public void addPoint(LatLng point)
-	{
-		points.add(point);
-	}
-	public void removePoint(int index)
-	{
-		points.remove(index);
-	}
-	public void clearPoints()
-	{
-		points.clear();
-	}
-	public boolean isEmpty()
-	{
-		return points.isEmpty();
-	}
+    public ArrayList<LatLng> getPoints()
+    {
+        return points;
+    }
+    public ArrayList<LatLng> getOriginalPoints()
+    {
+        return points;
+    }
+    public void setPoints(ArrayList<LatLng> list)
+    {
+        points = list;
+    }
+    public void addPoint(LatLng point)
+    {
+        points.add(point);
+    }
+    public void removePoint(int index)
+    {
+        points.remove(index);
+    }
+    public void clearPoints()
+    {
+        points.clear();
+    }
+    public boolean isEmpty()
+    {
+        return points.isEmpty();
+    }
 
-	public void outputPointsToOctave()
-	{
-		String output = "";
-		System.out.print("x=[");
-		for (LatLng a : points)
-		{
-			System.out.print(a.getLatitude() + ",");
-		}
-		System.out.print("]");
-		System.out.println("\n");
-		System.out.print("y=[");
-		for (LatLng a : points)
-		{
-			System.out.print(a.getLongitude() + ",");
-		}
-		System.out.print("]");
-		System.out.println("");
-		System.out.println("plot(x,y)");
-	}
+    public void outputPointsToOctave()
+    {
+        String output = "";
+        System.out.print("x=[");
+        for (LatLng a : points)
+        {
+            System.out.print(a.getLatitude() + ",");
+        }
+        System.out.print("]");
+        System.out.println("\n");
+        System.out.print("y=[");
+        for (LatLng a : points)
+        {
+            System.out.print(a.getLongitude() + ",");
+        }
+        System.out.print("]");
+        System.out.println("");
+        System.out.println("plot(x,y)");
+    }
 
 }
